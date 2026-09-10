61d0bb89994c59f77997494fda35d9984de1545a
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-10 20:15:40 +0530

    Phase G F3-unblock: ship 22 ACLs now that groups are shipped

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : Module : Studio-Misc',
-    'version': '17.0.0.0.48',
+    'version': '17.0.0.0.49',
     'summary': (
         'Catch-all landing zone for Studio-created artefacts that '
         'don\'t fit any of the domain-specific companion modules '
