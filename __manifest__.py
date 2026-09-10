665d85b53ac50bbec08fa04fb5a9715506da7b5a
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-10 18:57:13 +0530

    Phase F6 round 2: ship TEST APP 05 tree (153 menus)

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : Module : Studio-Misc',
-    'version': '17.0.0.0.42',
+    'version': '17.0.0.0.43',
     'summary': (
         'Catch-all landing zone for Studio-created artefacts that '
         'don\'t fit any of the domain-specific companion modules '
@@ -78,6 +78,7 @@ etc.).
         'account',
     ],
     'data': [
+        'data/menus_f6r2_test_app_05.xml',
         'data/rules_f7.xml',
         'data/menus_f6.xml',
         'data/server_actions_f5.xml',
