da8ac7900a666b14e5904fa3cdb484b3914ed6a8
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-11 15:36:50 +0530

    Views final hotfix: remap studio_customization parent xmlids to companion

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : Module : Studio-Misc',
-    'version': '17.0.0.0.57',
+    'version': '17.0.0.0.58',
     'summary': (
         'Catch-all landing zone for Studio-created artefacts that '
         'don\'t fit any of the domain-specific companion modules '
@@ -78,6 +78,7 @@ etc.).
         'account',
     ],
     'data': [
+        'views/views_final.xml',
         'data/menus_f6_final.xml',
         'data/server_actions_f5_mailpost.xml',
         'data/menus_f6_url.xml',
