4a28db2764030a698170958b644b6fb31e165a85
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-16 14:11:29 +0530

    Add studio_usermodel_migration to depends + take over x_vendor_group group_g ACL row (breaks cycle)

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Jinasena : Module : Studio-Misc',
-    'version': '17.0.0.0.75',
+    'version': '17.0.0.0.76',
     'summary': (
         'Catch-all landing zone for Studio-created artefacts that '
         'don\'t fit any of the domain-specific companion modules '
@@ -76,6 +76,7 @@ etc.).
         'sale',
         'purchase',
         'account',
+            'studio_usermodel_migration',
     ],
     'data': [
         'views/views_qweb_validated.xml',
