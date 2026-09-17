7c37c90359847f69a9ea2ad017462c82031f0f1d
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-17 11:02:23 +0530

    HOTFIX: strip broken self-referential related= clauses from Studio field ports (caused registry load failure)

--- models/ir_model_access_ext.py
+++ models/ir_model_access_ext.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class XIrModelAccessExt(models.Model):
     _inherit = 'ir.model.access'
 
-    x_studio_number_of_users = fields.Many2many('res.users', string='Users', readonly=True, related='group_id.users', store=False)
-    x_studio_user_name = fields.Char(string='User Name', readonly=True, related='group_id.users.complete_name', store=False)
+    x_studio_number_of_users = fields.Many2many('res.users', string='Users', readonly=True)
+    x_studio_user_name = fields.Char(string='User Name', readonly=True)
